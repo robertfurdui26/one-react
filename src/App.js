1088972bbeb8b73bd228c6d012cdbc62ae416c80
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-04-05 00:17:31 +0300

    more commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./Components/Header";
-//import Footer from "./Components/Footer";
+import Footer from "./Components/Footer";
 import Main from "./Components/Main";
+import "./style.css";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,52 @@
+const productData = [
+  {
+    id: 1,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=983&q=80",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+  {
+    id: 2,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+  {
+    id: 3,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+  {
+    id: 4,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+  {
+    id: 5,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+  {
+    id: 6,
+    name: "Cars training",
+    image:
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    price: "$200.99",
+    description: "Cars driving",
+  },
+];
+
+export default productData;
